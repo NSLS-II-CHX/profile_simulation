c1a8581789f4cf60023b1f2a6f8a399da675dba9
Maksim Rakitin <mrakitin@bnl.gov>
2018-04-27 11:59:12 -0400

    Add mpl stuff

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,6 +1,9 @@
+import matplotlib.pyplot as plt
 import databroker
 from databroker import Broker
 from srw_handler import SRWFileHandler
+
+plt.ion()
 
 db = Broker.named('chx-simulation')
 db.reg.register_handler('srw', SRWFileHandler, overwrite=True)
