84ea0d14f054c316e022f9bda92ba6f7e3ce1ce3
Maksim Rakitin <mrakitin@bnl.gov>
2018-04-27 11:58:34 -0400

    Add SRW handler to the profile

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,4 +1,7 @@
+import databroker
 from databroker import Broker
+from srw_handler import SRWFileHandler
 
 db = Broker.named('chx-simulation')
+db.reg.register_handler('srw', SRWFileHandler, overwrite=True)
 
