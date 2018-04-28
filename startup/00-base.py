f36c58db7691380a2b05750af157c166c38e5148
Maksim Rakitin <mrakitin@bnl.gov>
2018-04-28 09:17:30 -0400

    Fixed path to the default SRW file

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,7 +1,10 @@
+from pathlib import Path
 import matplotlib.pyplot as plt
 import databroker
 from databroker import Broker
 from srw_handler import SRWFileHandler, read_srw_file
+
+PROFILE_STARTUP_PATH = Path(get_ipython().profile_dir.startup_dir)
 
 plt.ion()
 
--- startup/01-srw-data.py
+++ startup/01-srw-data.py
@@ -1,6 +1,7 @@
 from srw_writer import SRWFileWriter
 
-srw_writer = SRWFileWriter('srw_writer', 'data/res_int_pr_se.dat', root_dir='/XF11ID/simulation', reg=db.reg)
+default_srw_file = str(PROFILE_STARTUP_PATH / Path('data/res_int_pr_se.dat'))
+srw_writer = SRWFileWriter('srw_writer', default_srw_file, root_dir='/XF11ID/simulation', reg=db.reg)
 srw_writer.read_attrs = ['image', 'mean', 'photon_energy']
 srw_writer.configuration_attrs = ['horizontal_extent', 'vertical_extent', 'shape']
 
