8ab9d8e31f2225f1cfa8b1a382b8bd7c2f73e95b
Maksim Rakitin <mrakitin@bnl.gov>
2018-04-28 09:21:52 -0400

    Added bluesky to the profile

--- startup/00-base.py
+++ startup/00-base.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import databroker
 from databroker import Broker
+from bluesky.run_engine import RunEngine
+import bluesky.plans as bp
 from srw_handler import SRWFileHandler, read_srw_file
 
 PROFILE_STARTUP_PATH = Path(get_ipython().profile_dir.startup_dir)
@@ -10,4 +12,7 @@ plt.ion()
 
 db = Broker.named('chx-simulation')
 db.reg.register_handler('srw', SRWFileHandler, overwrite=True)
+
+RE = RunEngine()
+RE.subscribe(db.insert)
 
